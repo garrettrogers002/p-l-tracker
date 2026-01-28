88e0beaa013aa81ebb702fe71a22f6cac7428198
Gary <garrett.rogers002@gmail.com>
2026-01-28 15:26:44 -0500

    apk export v1.1, slide over graph capability

---
+++ app/build/generated/hilt/component_sources/release/com/garrettrogers/pltracker/PLTrackerApp_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.garrettrogers.pltracker;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = PLTrackerApp.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface PLTrackerApp_GeneratedInjector {
+  void injectPLTrackerApp(PLTrackerApp pLTrackerApp);
+}
